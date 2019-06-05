d005aa7a635928d0c3ed7e6ce1c0b5f4f76a63b6
Merijn Hendriks <innohurrytocode@gmail.com>
2019-06-05 14:09:41 +0200

    Fix missing semicolon

--- server.js
+++ server.js
@@ -846,7 +846,7 @@ function handleRequest(req, body, url) {
 			FinalOutput = ReadJson('traderList.json');
 			break;
 		case "/client/server/list":
-			FinalOutput = '{"err":0, "errmsg":null, "data":[{"ip":"127.0.0.1", "port":1337}]}'
+			FinalOutput = '{"err":0, "errmsg":null, "data":[{"ip":"127.0.0.1", "port":1337}]}';
 			break;
 		case "/client/ragfair/search":
 			RagfairOffers(info);
