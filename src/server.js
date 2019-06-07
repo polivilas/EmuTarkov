adf1e4ef6750d5ab581becd1bfb25e5d4cd9bec5
Merijn Hendriks <innohurrytocode@gmail.com>
2019-06-07 18:01:26 +0200

    emulateServer has now effect on emulator behaviour

--- src/server.js
+++ src/server.js
@@ -10,66 +10,71 @@ var server = http.createServer();
 var port = settings.getPort();
 var output = "";
 
-server.on('request', function(req, resp) {
-	// Get the IP address of the client
-	output = "";
-	item.resetOutput();
-	
-	var remote = req.connection.remoteAddress;
-	
-	console.log('Got request from: %s for %s', remote, req.url);
-	
-	if (req.method == "POST") {
-		console.log("Posting");
-		req.on('data', function(data) {
-				zlib.inflate(data, function(error, body) {
-					if (error) {
-						console.log(error);
-					} else {
-						output = response.handleRequest(req, body.toString(), req.url);
-						
-						if (output == "DEAD") {
-							resp.writeHead(301, {Location: 'http://prod.escapefromtarkov.com'+req.url});
-							console.log("Redirecting");
-							resp.end();
+// get client requests
+if (settings.getEmulateServer()) {
+	server.on('request', function(req, resp) {
+		// Get the IP address of the client
+		output = "";
+		item.resetOutput();
+		
+		var remote = req.connection.remoteAddress;
+		
+		console.log('Got request from: %s for %s', remote, req.url);
+		
+		if (req.method == "POST") {
+			console.log("Posting");
+			req.on('data', function(data) {
+					zlib.inflate(data, function(error, body) {
+						if (error) {
+							console.log(error);
+						} else {
+							output = response.handleRequest(req, body.toString(), req.url);
+							
+							if (output == "DEAD") {
+								resp.writeHead(301, {Location: 'http://prod.escapefromtarkov.com'+req.url});
+								console.log("Redirecting");
+								resp.end();
+
+								return;
+							}
+
+							resp.writeHead(200, "OK", {'Content-Type': 'text/plain', 'content-encoding' : 'deflate', 'Set-Cookie' : 'PHPSESSID=yolo'});
+							
+							zlib.deflate(output, function(err, buf) {
+								resp.end(buf);
+							});
 
 							return;
 						}
-
-						resp.writeHead(200, "OK", {'Content-Type': 'text/plain', 'content-encoding' : 'deflate', 'Set-Cookie' : 'PHPSESSID=yolo'});
-						
-						zlib.deflate(output, function(err, buf) {
-							resp.end(buf);
-						});
-
-						return;
-					}
+				});
 			});
-		});
-	} else {
-		console.log("Getting");
-		output = response.handleRequest(req, "{}", req.url);
-		
-		if (output == "DEAD") {
-			resp.writeHead(301,	{Location: 'http://prod.escapefromtarkov.com'+req.url});
-			console.log("Redirecting");
-			resp.end();
+		} else {
+			console.log("Getting");
+			output = response.handleRequest(req, "{}", req.url);
+			
+			if (output == "DEAD") {
+				resp.writeHead(301,	{Location: 'http://prod.escapefromtarkov.com'+req.url});
+				console.log("Redirecting");
+				resp.end();
 
-			return;
+				return;
+			}
+
+			resp.writeHead(200, "OK", {'Content-Type': 'text/plain', 'content-encoding' : 'deflate', 'Set-Cookie' : 'PHPSESSID=yolo'});
+			
+			zlib.deflate(output, function(err, buf) {
+				resp.end(buf);
+			});
 		}
-
-		resp.writeHead(200, "OK", {'Content-Type': 'text/plain', 'content-encoding' : 'deflate', 'Set-Cookie' : 'PHPSESSID=yolo'});
-		
-		zlib.deflate(output, function(err, buf) {
-			resp.end(buf);
-		});
-	}
-});
+	});
+}
 
 // start the server
-server.listen(port, function() {
-	console.log('EmuTarkov listening on: %s', port);
-});
+if (settings.getEmulateServer()) {
+	server.listen(port, function() {
+		console.log('EmuTarkov listening on: %s', port);
+	});
+}
 
 // start the launcher
 if (settings.getEmulateLauncher()) {
