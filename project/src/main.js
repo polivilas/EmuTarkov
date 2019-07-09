1ea6d202bfc874d72a55ed00780b80ed5af80d33
Merijn Hendriks <innohurrytocode@gmail.com>
2019-07-10 00:43:03 +0200

    Add window title

--- project/src/main.js
+++ project/src/main.js
@@ -3,5 +3,12 @@
 const logger = require('./logger.js');
 const server = require('./server.js');
 
+function setTitle(title) {
+    process.stdout.write(
+        String.fromCharCode(27) + ']0;' + title + String.fromCharCode(7)
+    );
+}
+
+setTitle("Just EmuTarkov Server");
 logger.start();
 server.start();
