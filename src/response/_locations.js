4cc4b1a74f025ceb0dadfaecfa4c4fa9a3953548
MAOCI <33422088+themaoci@users.noreply.github.com>
2019-11-19 13:53:54 +0100

    Adding that Loot Generating function
    i rewrited it abit now it should work
    
    usage in _start.js > sendResponse > "MAPCONFIG"
    
    as `data_response = locations_f.LootGenerator(JSON.parse(data_response));`

--- src/response/_locations.js
+++ src/response/_locations.js
@@ -41,6 +41,26 @@ function prepareLocations() {
 	}
 }
 
+/* LootGenerator - tool used to generate new loot for api/location
+* input: location as object
+* output: location as object
+* */
+function LootGenerator(location)
+{
+    let itemsKeys_ = Object.keys(items.data);
+	for(let lootspawn in location.Loot)
+	{
+		if(location.Loot[lootspawn].IsStatic == false)
+		{
+			// its static place for loot we dont need to change main object _id for that so we just changing _tpl
+			location.Loot[lootspawn].Items[0]._tpl = itemsKeys_[utility.getRandomIntEx(itemsKeys_.length)]._id
+		}
+	}
+	//utility.writeJson("debug_map_generator.json",location.Loot);
+	return location;
+}
+
 //// ---- EXPORT LIST ---- ////
 
 module.exports.prepareLocations = prepareLocations;
+module.exports.LootGenerator = LootGenerator;
