744038f809e98bdff2a5d68ee58251c1912475e4
Merijn Hendriks <innohurrytocode@gmail.com>
2019-11-20 17:45:39 +0100

    Remove commented code from _build.js
    Icons are already overwritten

--- src/_build.js
+++ src/_build.js
@@ -1,12 +1,12 @@
 const childProcess = require('child_process');
-const UPX = require('upx')('best') // see options below
+const UPX = require('upx')('best')
 const { compile } = require('nexe');
 
 // compile the application
 console.log(">>>STARTING BUILD...");
 compile({
 	input: 'src/main.js',
-    output: 'out/JustEmuTarkov-Server',
+    	output: 'out/JustEmuTarkov-Server',
 	build: false,
 	ico: 'bin/icon.ico'
 }).then(function(err) {
@@ -26,29 +26,10 @@ compile({
 		console.log(">>>Compressing Executable...");
 		UPX('out/JustEmuTarkov-Server.exe')
 		.output('JustEmuTarkov-Server.exe')
-		.start().then(function(stats){
+		.start().then(function(stats) {
 			console.log(stats);
 		}).catch(function (err) {
 			console.log(err);
 		});
 	});
-	
-	
 });
-
-
-/*.then(function(err) {
-	// set the icon
-	childProcess.execFile('bin/ResourceHacker.exe', [
-		'-open',
-		'out/JustEmuTarkov-Server.exe',
-		'-save',
-		'out/JustEmuTarkov-Server.exe',
-		'-action',
-		'addoverwrite',
-		'-res',
-		'bin/icon.ico',
-		'-mask',
-		'ICONGROUP,MAINICON,'
-	], function(err) {});
-});*/
