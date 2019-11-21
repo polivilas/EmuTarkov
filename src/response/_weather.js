ec78251f2f5ed3e78daff3838ab742ea22aa692a
Merijn Hendriks <innohurrytocode@gmail.com>
2019-11-21 22:12:01 +0100

    Update weather.js

--- src/response/_weather.js
+++ src/response/_weather.js
@@ -1,5 +1,7 @@
 "use strict";
 
+require('./libs.js');
+
 const utility = require('../utility.js');
 
 //// ---- FUNCTIONS BELOW ---- ////
@@ -12,10 +14,9 @@ fallingWater // 0 - 1
 LyingWater // 0 - 1 
 Temperature -50 - 50
 */
+
 function main() { // TODO: Use weather API
-    let time = utility.getTime().replace("-", ":").replace("-", ":");
-    let date = utility.getDate();
-    let dateTime = date + " " + time;
+    let dateTime = utility.getTime().replace("-", ":").replace("-", ":") + " " + utility.getDate();
 
     return '{"err":0, "errmsg":null, "data":{"weather":{"timestamp":' + Math.floor(new Date() / 1000) + ', "cloud":-0.475, "wind_speed":2, "wind_direction":3, "wind_gustiness":0.081, "rain":1, "rain_intensity":0, "fog":0.002, "temp":14, "pressure":763, "date":"' + date + '", "time":"' + dateTime + '"}, "date":"' + date + '", "time":"' + time + '", "acceleration":1}}';
 }
@@ -23,4 +24,3 @@ function main() { // TODO: Use weather API
 //// ---- EXPORT LIST ---- ////
 
 module.exports.main = main;
-//module.exports.funcname = funcname; // preset
