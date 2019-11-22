247bdabcaeb294e46cea9dd7c52705a99bef602e
Merijn Hendriks <innohurrytocode@gmail.com>
2019-11-22 06:34:37 +0100

    Add icon

--- dev/build.js
+++ dev/build.js
@@ -8,10 +8,10 @@ compile({
 	input: 'src/main.js',
     	output: 'out/JustEmuTarkov-Server',
 	build: false,
-	ico: 'bin/icon.ico'
+	ico: 'dev/res/icon.ico'
 }).then(function(err) {
 	console.log(">>>Changing ICON...");
-	childProcess.execFile('bin/ResourceHacker.exe', [
+	childProcess.execFile('dev/bin/ResourceHacker.exe', [
 		'-open',
 		'out/JustEmuTarkov-Server.exe',
 		'-save',
