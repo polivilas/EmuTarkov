beee052cc52fb8dfe9a6bec2d60f2fb28c1ea4db
Merijn Hendriks <innohurrytocode@gmail.com>
2019-11-29 14:16:10 +0100

    Initial server cleanup
    It's time for the big server cleanup and refactoring!

--- scripts/utility.js
+++ scripts/utility.js
@@ -2,49 +2,41 @@
 
 require("./libs.js");
 
-// ----------- ONLY STATIC FUNCTIONS -------------- //
-
-function readJson(file) { //read json file with deleting all tabulators and new lines
+function readJson(file) {
     return (fs.readFileSync(file, 'utf8')).replace(/[\r\n\t]/g, '').replace(/\s\s+/g, '');
 }
 
-function writeJson(file, data) { //write json to file with tabulators and new lines
+function writeJson(file, data) {
     fs.writeFileSync(file, JSON.stringify(data, null, "\t"), 'utf8');
 }
 
-function clearString(s){
-	return s.replace(/[\r\n\t]/g, '').replace(/\s\s+/g, '').replace(/[\\]/g, "");
+function clearString(string) {
+	return string.replace(/[\r\n\t]/g, '').replace(/\s\s+/g, '').replace(/[\\]/g, "");
 }
 
-function adlerGen(s){
-	return adler32.sum(s);
+function adlerGen(string){
+	return adler32.sum(string);
 }
 
-function generateCRC(s){ // generate CRC from Server Version
-    return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+function generateCRC(string) {
+    return string.split("").reduce(function(a, b) {
+        a = ((a << 5) - a) + b.charCodeAt(0);
+        return a & a;
+    }, 0);
 }
 
-function getRandomInt(min = 0, max = 100) { // random number from given range
+function getRandomInt(min = 0, max = 100) {
     min = Math.ceil(min);
     max = Math.floor(max);
-
-    if (max > min) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    } else {
-        return min;
-    }
+    return (max > min) ? Math.floor(Math.random() * (max - min + 1) + min) : min;
 }
 
-function getRandomIntEx(max) { // random number from 1 to max if 1 given return 1
-    if (max > 1) {
-        return Math.floor(Math.random() * (max - 2) + 1);
-    } else {
-        return 1;
-    }
+function getRandomIntEx(max) {
+    return (max > 1) ? Math.floor(Math.random() * (max - 2) + 1) : 1;
 }
 
 function removeDir(dir) {
-    fs.readdirSync(dir).forEach(function (file, index) {
+    for (file of fs.readdirSync(dir)) {
         let curPath = path.join(dir, file);
 
         if (fs.lstatSync(curPath).isDirectory()) {
@@ -52,7 +44,7 @@ function removeDir(dir) {
         } else {
             fs.unlinkSync(curPath);
         }
-    });
+    }
 
     fs.rmdirSync(dir);
 }
@@ -80,12 +72,9 @@ function getDate() {
     return today.getFullYear() + "-" + month + "-" + day;
 }
 
-/**
- * @return {string}
- */
-function MakeSign(length) {
-    let result           = '';
-    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function makeSign(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let charactersLength = characters.length;
     
     for (let i = 0; i < length; i++ ) {
@@ -95,7 +84,7 @@ function MakeSign(length) {
     return result;
 }
 
-function generateNewItemId() { // --> Generate ID ultra not repeatable tested for 10.000.000 generates on same time it wasnt duplicated so im assume its ok
+function generateNewItemId() {
 	let getTime = new Date();
 	let month = getTime.getMonth().toString();
 	let date = getTime.getDate().toString();
@@ -104,7 +93,7 @@ function generateNewItemId() { // --> Generate ID ultra not repeatable tested fo
 	let second = getTime.getSeconds().toString();
 	let random = getRandomInt(1000000000, 9999999999).toString();
 	let retVal = "I" + (month + date + hour + minute + second + random).toString();
-    let sign = MakeSign(24 - retVal.length).toString();
+    let sign = makeSign(24 - retVal.length).toString();
     
     return retVal + sign;
 }
@@ -126,18 +115,17 @@ function getLocalIpAddress() {
     return address;
 }
 
-// Module exporting
-module.exports.getLocalIpAddress = getLocalIpAddress;
+module.exports.readJson = readJson;
+module.exports.writeJson = writeJson;
 module.exports.clearString = clearString;
 module.exports.adlerGen = adlerGen;
 module.exports.generateCRC = generateCRC;
-module.exports.readJson = readJson;
-module.exports.writeJson = writeJson;
 module.exports.getRandomInt = getRandomInt;
 module.exports.getRandomIntEx = getRandomIntEx;
-module.exports.generateNewItemId = generateNewItemId;
 module.exports.removeDir = removeDir;
 module.exports.getTimestamp = getTimestamp;
 module.exports.getTime = getTime;
 module.exports.getDate = getDate;
-module.exports.MakeSign = MakeSign;
+module.exports.makeSign = makeSign;
+module.exports.generateNewItemId = generateNewItemId;
+module.exports.getLocalIpAddress = getLocalIpAddress;
