501aeaea878e8d3c03d056e90a7f7e3ab8e5be3d
BALIST0N <BALIST0N@users.noreply.github.com>
2019-12-06 14:39:01 +0100

    workaround for weapons build bug
    not clean but kinda working

--- scripts/item/_weaponBuilds.js
+++ scripts/item/_weaponBuilds.js
@@ -13,6 +13,46 @@ function SaveBuild(tmpList, body) {
 
 	let savedBuilds = JSON.parse(utility.readJson(getUserBuildsPath()));
 
+	//verif duplicated ids
+	let buildAsString = JSON.stringify(body);
+
+	let ids = [];
+	for(let itemBuild of body.items){ ids.push(itemBuild._id); }
+
+	let dupes = {};
+	ids.forEach(function(x){ dupes[x] = (dupes[x] || 0)+1; });
+
+	let newParents = {}
+	for(let itemBuilds in body.items)
+	{
+		if(dupes[body.items[itemBuilds]._id] > 1 )
+		{
+			console.log("id is duplicated ! " + body.items[itemBuilds]._id);
+			let newId = utility.generateNewItemId();
+			newParents[newId] =
+			{
+				"oldId" : body.items[itemBuilds]._id,
+				"slot" : body.items[itemBuilds].slotId
+			};
+
+			body.items[itemBuilds]._id = newId;
+
+		}
+		if(dupes[body.items[itemBuilds].parentId] > 1)
+		{
+			console.log("an item has a duplicated parent ! : " + body.items[itemBuilds].parentId);
+			for(let newId in newParents)
+			{
+				if(body.items[itemBuilds].parentId == newParents[newId].oldId)
+				{
+					body.items[itemBuilds].parentId = newId;
+				}
+				delete newParents[newId];
+			}
+		}
+
+	}
+
 	savedBuilds.data.push(body);
 	utility.writeJson(getUserBuildsPath(), savedBuilds);
 	item.resetOutput();
