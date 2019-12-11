6811674bfecab564b88064a340c5e240e6752567
Merijn Hendriks <innohurrytocode@gmail.com>
2019-12-11 20:04:14 +0100

    Set examined item to true when viewed in encyclopedia

--- scripts/item/_examine.js
+++ scripts/item/_examine.js
@@ -4,12 +4,14 @@ const utility = require('../utility.js');
 const profile = require('../profile.js');
 
 function examineItem(tmpList, body) {
-	tmpList.data[0].Encyclopedia[body.item] = true;
+	tmpList.data[0].Encyclopedia[body.item] = false;
 	profile.setCharacterData(tmpList);
 	return "OK";
 }
 
 function readEncyclopedia(tmpList, body) {
+	tmpList.data[0].Encyclopedia[body.ids[0]] = true;
+	profile.setCharacterData(tmpList);
 	return "OK";
 }
 
