24838c91780b96677ae8882fb5c7df4993e23c79
Merijn Hendriks <innohurrytocode@gmail.com>
2019-12-30 05:55:03 +0100

    Cache items.json only

--- src/caching/_cache.js
+++ src/caching/_cache.js
@@ -44,7 +44,7 @@ function genericCacher(cachename, filepathNode, output = "") {
     }
 
     if (typeof base.crc != "undefined") {
-        base.crc = utility.generateCRC(json.stringify(base));
+        base.crc = utility.generateCRC(json.stringify(base.data));
     }
     
     json.write("user/cache/" + cachename, base);
