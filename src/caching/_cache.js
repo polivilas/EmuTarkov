9148085d24c52c734ea7c3dc6a6fb7f8dc66cca3
Merijn Hendriks <innohurrytocode@gmail.com>
2019-12-30 05:50:45 +0100

    CRC was minus

--- src/caching/_cache.js
+++ src/caching/_cache.js
@@ -44,7 +44,7 @@ function genericCacher(cachename, filepathNode, output = "") {
     }
 
     if (typeof base.crc != "undefined") {
-        base.crc = utility.generateCRC(json.stringify(base.data));
+        base.crc = utility.generateCRC(json.stringify(base));
     }
     
     json.write("user/cache/" + cachename, base);
