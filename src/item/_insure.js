c177dffbc7e856acc41ea862a487442dc2192911
Merijn Hendriks <innohurrytocode@gmail.com>
2020-01-03 04:52:14 +0100

    Add insurance price calculation

--- src/item/_insure.js
+++ src/item/_insure.js
@@ -4,18 +4,26 @@ require('../libs.js');
 
 function cost(info) {
     let output = {"err":0,"errmsg":null,"data":{}};
+    let tmpList = profile.getCharacterData();
 
     for (let trader of info.traders) {
         let items = {};
 
-        for (let item of info.items) {
-            items[item] = 100;
+        for (let key of info.items) {
+            for (let item of tmpList.data[0].Inventory.items) {
+                if (item._id === key) {
+                    let template = json.parse(json.read(filepaths.templates.items[item._tpl]));
+
+                    items[template.Id] = round(template.Price * 0.65);
+                    break;
+                }
+            }
         }
 
         output.data[trader] = items;
     }
 
-    return output;
+    return json.stringify(output);
 }
 
 function insure(tmpList, body) {
