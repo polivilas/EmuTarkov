771662c0869600a4b3ba4d8b07f275583c470588
Merijn Hendriks <innohurrytocode@gmail.com>
2020-01-08 12:15:10 +0100

    Rewrite bots system

--- src/bots.js
+++ src/bots.js
@@ -2,86 +2,86 @@
 
 function getRandomValue(node) {
 	let keys = Object.keys(node);
-	return node[keys[utility.getRandomInt(0, keys.length - 1)]];
+	return json.parse(json.read(node[keys[utility.getRandomInt(0, keys.length - 1)]]));
 }
 
-function getBotNode(role) {
-	if (role === "bear") {
-		return filepaths.bots.pmc.bear;
-	} else if (role === "usec") {
-		return filepaths.bots.pmc.usec;
-	} else if (role === "assault") {
-		return filepaths.bots.scav.assault;
-	} else if (role === "bossBully") {
-		return filepaths.bots.scav.bossbully;
-	} else if (role === "bossGluhar") {
-		return filepaths.bots.scav.bossgluhar;
-	} else if (role === "bossKilla") {
-		return filepaths.bots.scav.bosskilla;
-	}  else if (role === "bossKojaniy") {
-		return filepaths.bots.scav.bosskojaniy;
-	} else if (role === "followerBully") {
-		return filepaths.bots.scav.followerbully;
-	} else if (role === "followerGluharAssault") {
-		return filepaths.bots.scav.followergluharassault;
-	} else if (role === "followerGluharScout") {
-		return filepaths.bots.scav.followergluharscout;
-	} else if (role === "followerGluharSecurity") {
-		return filepaths.bots.scav.followergluharsecurity;
-	} else if (role === "followerKojaniy") {
-		return filepaths.bots.scav.followerkojaniy;
-	} else if (role === "marksman") {
-		return filepaths.bots.scav.marksman;
-	} else if (role === "pmcBot") {
-		return filepaths.bots.scav.pmcbot;
-	}
+function getBotNode(type) {
+	switch (type) {
+		case "bear":
+			return filepaths.bots.pmc.bear;
 
-	return {};
+		case "usec":
+			return filepaths.bots.pmc.usec;
+
+		case "assault":
+			return filepaths.bots.scav.assault;
+
+		case "bossBully":
+			return filepaths.bots.scav.bossbully;
+	
+		case "bossGluhar":
+			return filepaths.bots.scav.bossgluhar;
+
+		case "bossKilla":
+			return filepaths.bots.scav.bosskilla;
+
+		case "bossKojaniy":
+			return filepaths.bots.scav.bosskojaniy;
+	
+		case "followerBully":
+			return filepaths.bots.scav.followerbully;
+
+		case "followerGluharAssault":
+			return filepaths.bots.scav.followergluharassault
+
+		case "followerGluharScout":
+			return filepaths.bots.scav.followergluharscout;
+	
+		case "followerGluharSecurity":
+			return filepaths.bots.scav.followergluharsecurity;
+
+		case "followerKojaniy":
+			return filepaths.bots.scav.followerkojaniy;
+
+		case "marksman":
+			return filepaths.bots.scav.marksman;
+	
+		case "pmcBot":
+			return filepaths.bots.scav.pmcbot;
+
+		default:
+			console.log("BOTS ERROR: SHITS FUCKED, PLZ SEND HELP");
+			return {};
+	}
 }
 
 function addDogtag(botBase) {
-	if (botBase.Info.Side !== 'Savage') {
-		if (botBase.Info.Side === 'Usec') {
-			botBase.Inventory.items.push({
-				_id: "dogtag_" + 100000000 + utility.getRandomIntEx(899999999),
-				_tpl: "59f32c3b86f77472a31742f0",
-				parentId: botBase.Inventory.equipment,
-				slotId: "Dogtag",
-				upd: {
-					"Dogtag": {
-						"Nickname": botBase.Info.Nickname,
-						"Side": botBase.Info.Side,
-						"Level": botBase.Info.Level,
-						"Time": "2020-01-01T00:00:00",
-						"Status": "Killed by JET",
-						"KillerName": 'JustEmuTarkov',
-						"WeaponName": "JET Reverse Engineering"
-					},
-					"SpawnedInSession": "true"
-				}
-			})
-		} else {
-			botBase.Inventory.items.push({
-				_id: "dogtag_" + 100000000 + utility.getRandomIntEx(899999999),
-				_tpl: "59f32bb586f774757e1e8442",
-				parentId: botBase.Inventory.equipment,
-				slotId: "Dogtag",
-				upd: {
-					"Dogtag": {
-						"Nickname": botBase.Info.Nickname,
-						"Side": botBase.Info.Side,
-						"Level": botBase.Info.Level,
-						"Time": "2020-01-01T00:00:00",
-						"Status": "Killed by JET",
-						"KillerName": 'JustEmuTarkov',
-						"WeaponName": "JET Reverse Engineering"
-					},
-					"SpawnedInSession": "true"
-				}
-			})
+	let dogtagItem = {
+		_id: "dogtag_" + 100000000 + utility.getRandomIntEx(899999999),
+		_tpl: "__REPLACEME__",
+		parentId: botBase.Inventory.equipment,
+		slotId: "Dogtag",
+		upd: {
+			"Dogtag": {
+				"Nickname": botBase.Info.Nickname,
+				"Side": botBase.Info.Side,
+				"Level": botBase.Info.Level,
+				"Time": "2020-01-01T00:00:00",
+				"Status": "Killed by JET",
+				"KillerName": 'JustEmuTarkov',
+				"WeaponName": "JET Reverse Engineering"
+			},
+			"SpawnedInSession": "true"
 		}
 	}
 
+	if (botBase.Info.Side === 'Usec') {
+		dogtagItem._tpl = "59f32c3b86f77472a31742f0";
+	} else {
+		dogtagItem._tpl = "59f32bb586f774757e1e8442";
+	}
+
+	botBase.Inventory.items.push(dogtagItem);
 	return botBase;
 }
 
@@ -89,11 +89,11 @@ function generateBot(botBase, role) {
 	let type = role;
 
 	if (role === "cursedAssault") {
-		role = "assault"
+		type = "assault"
 	}
 
 	// chance to spawn simulated PMC players
-	if (((role === "assault" || role === "marksman" || role === "pmcBot") && settings.gameplay.bots.pmcEnabled)) {
+	if (((type === "assault" || type === "marksman" || type === "pmcBot") && settings.gameplay.bots.pmcEnabled)) {
 		let spawnChance = utility.getRandomInt(0, 99);
 		let sideChance = utility.getRandomInt(0, 99);
 		botBase.Info.Level = Math.floor(Math.random()*Math.floor(70));
@@ -111,21 +111,20 @@ function generateBot(botBase, role) {
 
 	let botNode = getBotNode(type);
 
+	// generate bot
 	botBase.Info.Settings.Role = role;
-	botBase.Info.Nickname = json.parse(json.read(getRandomValue(botNode.names)));
-	botBase.Info.Settings.Experience = json.parse(json.read(getRandomValue(botNode.experience)));
-	botBase.Info.Voice = json.parse(json.read(getRandomValue(botNode.appearance.voice)));
-	botBase.Health = json.parse(json.read(getRandomValue(botNode.health)));
-	botBase.Customization.Head = json.parse(json.read(getRandomValue(botNode.appearance.head)));
-	botBase.Customization.Body = json.parse(json.read(getRandomValue(botNode.appearance.body)));
-	botBase.Customization.Feet = json.parse(json.read(getRandomValue(botNode.appearance.feet)));
-	botBase.Customization.Hands = json.parse(json.read(getRandomValue(botNode.appearance.hands)));
-	botBase.Inventory = json.parse(json.read(getRandomValue(botNode.inventory)));
+	botBase.Info.Nickname = getRandomValue(botNode.names);
+	botBase.Info.Settings.Experience = getRandomValue(botNode.experience);
+	botBase.Info.Voice = getRandomValue(botNode.appearance.voice);
+	botBase.Health = getRandomValue(botNode.health);
+	botBase.Customization.Head = getRandomValue(botNode.appearance.head);
+	botBase.Customization.Body = getRandomValue(botNode.appearance.body);
+	botBase.Customization.Feet = getRandomValue(botNode.appearance.feet);
+	botBase.Customization.Hands = getRandomValue(botNode.appearance.hands);
+	botBase.Inventory = getRandomValue(botNode.inventory);
 
-	// 35% to add add dogtag to scavs, 100% for PMC's
-	let dogtagChance = utility.getRandomInt(0, 99);
-		
-	if (dogtagChance < 35 || type === "usec" || type === "bear" || type === "pmcBot") {
+	// add dogtag to PMC's		
+	if (type === "usec" || type === "bear") {
 		botBase = addDogtag(botBase);
 	}
 	
