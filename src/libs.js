c78703a7d40d34e760b1e8525ae9ee93f6c9a293
Merijn Hendriks <innohurrytocode@gmail.com>
2020-01-16 05:28:05 +0100

    Add offraid to lib

--- src/libs.js
+++ src/libs.js
@@ -66,6 +66,7 @@ module.exports = function(isFirstLaunch = false, time = 0) {
 	global.index_f = require('./response/_homeCredits.js');
 	global.assort_f = require('./response/_assort.js');
 	global.keepAlive_f = require('./response/_keepAlive.js');
+	global.offraid_f = require('./response/_offraid.js');
 	global.server = require('./server/_start.js');
 	global.constants = require('./server/_constants.js');
 	global.header_f = require('./server/_sendHeader.js');
