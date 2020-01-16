67ef59125ce9bbad3e51d995ed0fb062ca0ab42f
Merijn Hendriks <innohurrytocode@gmail.com>
2020-01-16 14:11:25 +0100

    Simplify bots.js

--- src/response/_bots.js
+++ src/response/_bots.js
@@ -8,66 +8,26 @@ function getRandomValue(node) {
 }
 
 function getBotNode(type) {
-	switch (type) {
-		case "bear":
-			return filepaths.bots.pmc.bear;
+	type = type.toLowerCase();
 
-		case "usec":
-			return filepaths.bots.pmc.usec;
-
-		case "assault":
-			return filepaths.bots.scav.assault;
-
-		case "bossBully":
-			return filepaths.bots.scav.bossbully;
-	
-		case "bossGluhar":
-			return filepaths.bots.scav.bossgluhar;
-
-		case "bossKilla":
-			return filepaths.bots.scav.bosskilla;
-
-		case "bossKojaniy":
-			return filepaths.bots.scav.bosskojaniy;
-	
-		case "followerBully":
-			return filepaths.bots.scav.followerbully;
-
-		case "followerGluharAssault":
-			return filepaths.bots.scav.followergluharassault
-
-		case "followerGluharScout":
-			return filepaths.bots.scav.followergluharscout;
-	
-		case "followerGluharSecurity":
-			return filepaths.bots.scav.followergluharsecurity;
-
-		case "followerKojaniy":
-			return filepaths.bots.scav.followerkojaniy;
-
-		case "marksman":
-			return filepaths.bots.scav.marksman;
-	
-		case "pmcBot":
-			return filepaths.bots.scav.pmcbot;
-
-		default:
-			logger.logError("bots type " + type + " is not handled");
-			return {};
+	if (type === "bear" || type === "usec") {
+		return filepaths.bots.pmc[type];
 	}
+
+	return filepaths.bots.scav[type];
 }
 
-function addDogtag(botBase) {
+function addDogtag(bot) {
 	let dogtagItem = {
 		_id: "dogtag_" + 100000000 + utility.getRandomIntEx(899999999),
 		_tpl: "__REPLACEME__",
-		parentId: botBase.Inventory.equipment,
+		parentId: bot.Inventory.equipment,
 		slotId: "Dogtag",
 		upd: {
 			"Dogtag": {
-				"Nickname": botBase.Info.Nickname,
-				"Side": botBase.Info.Side,
-				"Level": botBase.Info.Level,
+				"Nickname": bot.Info.Nickname,
+				"Side": bot.Info.Side,
+				"Level": bot.Info.Level,
 				"Time": "2020-01-01T00:00:00",
 				"Status": "Killed by JET",
 				"KillerName": 'JustEmuTarkov',
@@ -77,89 +37,91 @@ function addDogtag(botBase) {
 		}
 	}
 
-	if (botBase.Info.Side === 'Usec') {
+	if (bot.Info.Side === 'Usec') {
 		dogtagItem._tpl = "59f32c3b86f77472a31742f0";
 	} else {
 		dogtagItem._tpl = "59f32bb586f774757e1e8442";
 	}
 
-	botBase.Inventory.items.push(dogtagItem);
-	return botBase;
+	bot.Inventory.items.push(dogtagItem);
+	return bot;
 }
 
-function removeSecureContainer(botBase) {
+function removeSecureContainer(bot) {
 	let idsToRemove = [];
 
-	for (let item of botBase.Inventory.items) {
+	for (let item of bot.Inventory.items) {
         if (item.slotId === "SecuredContainer") {
-			idsToRemove = itm_hf.findAndReturnChildren(botBase, item._id);
+			idsToRemove = itm_hf.findAndReturnChildren(bot, item._id);
         }
 	}
 	
 	if (idsToRemove.length > 0) {
 		for (let itemId of idsToRemove) {
-			for (let index in botBase.Inventory.items) {
-				if (botBase.Inventory.items[index]._id === itemId) {
-					botBase.Inventory.items.splice(index, 1);
+			for (let index in bot.Inventory.items) {
+				if (bot.Inventory.items[index]._id === itemId) {
+					bot.Inventory.items.splice(index, 1);
 				}
 			}
 		}
 	}
 
-	return botBase;
+	return bot;
 }
 
-function generateBot(botBase, role) {
+function generateBot(bot, role) {
 	let type = role;
 
 	if (role === "cursedAssault") {
-		type = "assault"
+		type = "assault";
 	}
 
 	// chance to spawn simulated PMC players
 	if (((type === "assault" || type === "marksman" || type === "pmcBot") && settings.gameplay.bots.pmcEnabled)) {
 		let spawnChance = utility.getRandomInt(0, 99);
 		let sideChance = utility.getRandomInt(0, 99);
-		botBase.Info.Level = Math.floor(Math.random()*Math.floor(70));
-		
+
 		if (spawnChance < settings.gameplay.bots.pmcSpawnChance) {
 			if (sideChance < settings.gameplay.bots.pmcUsecChance) {
-				botBase.Info.Side = "Usec";
+				bot.Info.Side = "Usec";
 				type = "usec";
 			} else {
-				botBase.Info.Side = "Bear";
+				bot.Info.Side = "Bear";
 				type = "bear";
 			}
+
+			bot.Info.Level = utility.getRandomInt(1, 70);
 		}
 	}
 
+	// we don't want player scav to be generated as PMC
 	if (role === "playerScav") {
 		type = "assault";
 	}
 
+	// generate bot
 	let botNode = getBotNode(type);
 
-	// generate bot
-	botBase.Info.Settings.Role = role;
-	botBase.Info.Nickname = getRandomValue(botNode.names);
-	botBase.Info.Settings.Experience = getRandomValue(botNode.experience);
-	botBase.Info.Voice = getRandomValue(botNode.appearance.voice);
-	botBase.Health = getRandomValue(botNode.health);
-	botBase.Customization.Head = getRandomValue(botNode.appearance.head);
-	botBase.Customization.Body = getRandomValue(botNode.appearance.body);
-	botBase.Customization.Feet = getRandomValue(botNode.appearance.feet);
-	botBase.Customization.Hands = getRandomValue(botNode.appearance.hands);
-	botBase.Inventory = getRandomValue(botNode.inventory);
+	bot.Info.Settings.Role = role;
+	bot.Info.Nickname = getRandomValue(botNode.names);
+	bot.Info.Settings.Experience = getRandomValue(botNode.experience);
+	bot.Info.Voice = getRandomValue(botNode.appearance.voice);
+	bot.Health = getRandomValue(botNode.health);
+	bot.Customization.Head = getRandomValue(botNode.appearance.head);
+	bot.Customization.Body = getRandomValue(botNode.appearance.body);
+	bot.Customization.Feet = getRandomValue(botNode.appearance.feet);
+	bot.Customization.Hands = getRandomValue(botNode.appearance.hands);
+	bot.Inventory = getRandomValue(botNode.inventory);
 
 	// remove secure container
-	botBase = removeSecureContainer(botBase);
+	bot = removeSecureContainer(bot);
 
 	// add dogtag to PMC's		
 	if (type === "usec" || type === "bear") {
-		botBase = addDogtag(botBase);
+		bot = addDogtag(bot);
 	}
 	
-	return botBase;
+	return bot;
 }
 
 function generate(databots) {
@@ -180,10 +142,10 @@ function generate(databots) {
 }
 
 function generatePlayerScav() {
-	let playerscav = generate({"conditions":[{"Role":"playerScav","Limit":1,"Difficulty":"normal"}]}).data;
+	let scavData = generate({"conditions":[{"Role":"playerScav","Limit":1,"Difficulty":"normal"}]}).data;
 
-	playerscav[0].Info.Settings = {};
-	return playerscav[0];
+	scavData[0].Info.Settings = {};
+	return scavData[0];
 }
 
 module.exports.generate = generate;
