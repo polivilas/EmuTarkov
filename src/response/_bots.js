aacd36d2123f1272f0d4b5debaea10c508a7d093
Merijn Hendriks <innohurrytocode@gmail.com>
2020-01-22 22:26:13 +0100

    Fix ID generation issue

--- src/response/_bots.js
+++ src/response/_bots.js
@@ -9,7 +9,7 @@ function getRandomValue(node) {
 
 function addDogtag(bot) {
 	let dogtagItem = {
-		_id: generateNewItemId(),
+		_id: utility.generateNewItemId(),
 		_tpl: ((bot.Info.Side === 'Usec') ? "59f32c3b86f77472a31742f0" : "59f32bb586f774757e1e8442"),
 		parentId: bot.Inventory.equipment,
 		slotId: "Dogtag",
